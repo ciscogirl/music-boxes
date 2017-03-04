7a9d42eb72e050348cd7d2c6f14393b90ce9af18
ciscogirl <alethiadahlin@gmail.com>
2017-03-04 13:49:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var url= 'http://bcw.blob.core.windows.net/public/img/sounds/scale-00'
+var url= 'http://bcw.blob.core.windows.net/public/img/sounds/scale-0'
 
 var boombox= document.getElementById('boombox')
 
 function playTune(num){
+  
+  if(num < 10){
+    tuneUrl += '0'
+    
+  }
+  tuneUrl + Url += num + '.mp3'
   
   var tuneUrl = url + num + '.mp3'
   console.log(tuneUrl)
