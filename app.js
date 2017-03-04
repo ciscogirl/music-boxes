006cd98a1709a24da464315c7861146c51d8b0ca
ciscogirl <alethiadahlin@gmail.com>
2017-03-04 14:03:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var url= 'http://bcw.blob.core.windows.net/public/img/sounds/scale-0'
+var url = 'http://bcw.blob.core.windows.net/public/img/sounds/scale-0';
 
-var boombox= document.getElementById('boombox')
+var boombox= document.getElementById('boombox');
 
 function playTune(num){
   
   if(num < 10){
-    tuneUrl += '0'
+    tuneUrl += '0';
     
   }
-  tuneUrl + Url += num + '.mp3'
+  tuneUrl += num + '.mp3';
   
-  var tuneUrl = url + num + '.mp3'
-  console.log(tuneUrl)
+  var tuneUrl = url + num + '.mp3';
+  console.log(tuneUrl);
   
-  boombox.src= tuneUrl
-  boombox.play()
+  boombox.src= tuneUrl;
+  boombox.play();
   
 }
